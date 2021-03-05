5d7ddc0f9ba2b6659e974845985ded5b868618d6
yeyuting <2728095838@qq.com>
2021-03-05 11:55:17 +0800

    role

---
+++ src/main/java/library/system/dto/SysUser.java
@@ -0,0 +1,84 @@
+package library.system.dto;
+
+import java.util.List;
+
+/**
+ * @author yeyuting
+ * @create 2021/2/25
+ */
+public class SysUser {
+
+    Integer id ;
+
+    String userName ;
+
+    String userId ;
+
+    String userPassword ;
+
+    String token ;
+
+    SysRole sysRole ;
+
+    List<SysPrivilege> privilegeList ;
+
+    String extendedField2 ;
+
+    String extendedField3 ;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public SysRole getSysRole() {
+        return sysRole;
+    }
+
+    public void setSysRole(SysRole sysRole) {
+        this.sysRole = sysRole;
+    }
+
+    public List<SysPrivilege> getPrivilegeList() {
+        return privilegeList;
+    }
+
+    public void setPrivilegeList(List<SysPrivilege> privilegeList) {
+        this.privilegeList = privilegeList;
+    }
+}
